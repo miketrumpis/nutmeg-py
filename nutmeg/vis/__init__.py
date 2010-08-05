4da7936eca7a57334815224fd78eaab7feed97b1
miketrumpis <mtrumpis@gmail.com>
2010-08-05 14:21:05 -0700

    small changes to scripts; add a gitignore

--- nutmeg/vis/__init__.py
+++ nutmeg/vis/__init__.py
@@ -2,6 +2,11 @@ __docformat__ = 'restructuredtext'
 import os, sys
 from PyQt4 import QtGui
 
+## if QtGui.QApplication.startingUp():
+##     global_app = QtGui.QApplication(sys.argv)
+## else:
+##     global_app = QtGui.QApplication.instance()
+
 # register the nutmeg plugin before launching any xip viewers
 from xipy.overlay.plugins import register_overlay_plugin, \
      all_registered_plugins
@@ -12,31 +17,25 @@ register_overlay_plugin(NmTimeFreqWindow)
 
 from nutmeg.vis.tfstats_manager import TimeFreqSnPMaps
 
-import nipy.core.api as ni_api
-
 import xipy.volume_utils as vu
-from xipy.io import load_image
+from xipy.io import load_spatial_image
 
 def plot_tfbeam_3d(beam):
     pass
 
 def plot_tfbeam(beam, stats=None, with3d=False):
     from xipy.vis.ortho_viewer import ortho_viewer
-    struct = load_image(beam.coreg.mrpath)
+    struct = load_spatial_image(beam.coreg.mrpath)
     sman = TimeFreqSnPMaps(stats_results=stats)
     bbox = vu.world_limits(struct)
     bman = TFBeamManager(bbox, bstats_manager=sman)
     bman.update_beam(beam)
 
-    if QtGui.QApplication.startingUp():
-        app = QtGui.QApplication(sys.argv)
-    else:
-        app = QtGui.QApplication.instance()
 
     win = ortho_viewer(image=struct, mayavi_viewer=with3d)
     win.make_tool_from_functional_manager(NmTimeFreqWindow, bman)
     win.show()
-    app.exec_()
+##     app.exec_()
 ##     # XYZ: this is truly ugly
     bman.signal_image_props()
     bman.signal_new_image()
@@ -46,8 +45,6 @@ def with_attribute(a):
     def dec(f):
         def runner(obj, *args, **kwargs):
             if not getattr(obj, a, False):
-##                 print 'function %s not available until %s is true'%(f.func_name,
-##                                                                     a)
                 return
             return f(obj, *args, **kwargs)
         # copy f's info to runner
